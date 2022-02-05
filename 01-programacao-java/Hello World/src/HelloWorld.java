10eb66fb6516c9783f121f0094492ceb6ab4f47b
Orlewilson B. Maia <prof.orlewilson@gmail.com>
2022-02-05 09:26:57 -0400

    adding Hello World
    Signed-off-by: Orlewilson B. Maia <prof.orlewilson@gmail.com>

---
+++ 01-programacao-java/Hello World/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Revisão de Programação
+ * Orlewilson Maia
+ * 04/02/2022
+ * Primeiro programa em Java.
+ * 
+ * */
+
+public class HelloWorld {
+
+	// método principal
+	public static void main(String[] args) {
+
+		// mostrar mensagem no console
+		System.out.println("Hello World, Java!");
+	}
+}
