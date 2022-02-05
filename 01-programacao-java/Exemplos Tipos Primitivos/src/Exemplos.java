0746eac8b81e0fb21d06d4ce64a8b5228e208fd9
Orlewilson B. Maia <prof.orlewilson@gmail.com>
2022-02-05 09:27:29 -0400

    adding Exemplos Tipos Primitivos
    Signed-off-by: Orlewilson B. Maia <prof.orlewilson@gmail.com>

---
+++ 01-programacao-java/Exemplos Tipos Primitivos/src/Exemplos.java
@@ -0,0 +1,44 @@
+/*
+ * Revisão de Programação
+ * Orlewilson Maia
+ * 04/02/2022
+ * Exemplos de tipos primitivos em Java.
+ * 
+ * */
+
+public class Exemplos {
+
+	public static void main(String[] args) {
+
+		// declaração de variáveis
+		boolean a = false;
+		int b, c;
+		b = 2;
+		c = 3;
+
+		double d = 2.2;
+		float e = 3.2f;
+		short f = 2;
+
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("c: " + c);
+		System.out.println("d: " + d);
+		System.out.println("e: " + e);
+		System.out.println("f: " + f);
+
+		// valor formatado
+		double pi = 3.1415;
+		System.out.printf("pi: %.3f\n", pi);
+
+		char g = 'g';
+
+		// Tipo String
+		String nome = "Orlewilson";
+
+		System.out.println("Nome: " + nome);
+		System.out.println("Tamanho: " + nome.length());
+		System.out.println("Caracter posição 2: " + nome.charAt(2));
+		System.out.println("Comparação: " + nome.compareTo("Orlewilson"));
+	}
+}
